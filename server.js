cfbcf34ebb7c11b7425594d6ae153c79bf212786
Andrew Quach <andrewrquach@gmail.com>
2023-04-26 21:44:42 -0700

    feat: Configure app with dotenv and EJS engine, create home routes and controller, and add index, header, and footer views
    This commit sets up the app's configuration with dotenv to manage environment variables and the EJS templating engine to render views. It also creates the necessary home routes and controllers to handle incoming requests and displays the header, footer, and index views. This change will help the app display dynamic content and improve its overall functionality.

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const homeRoutes = require("./routes/home");
+
+//Load .env file in config folder
+dotenv.config({ path: "./config/config.env" }); // load config
+
+//Static Folder
+app.use(express.static("public"));
+
+//Using EJS for views
+app.set("view engine", "ejs");
+
+//Body Parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Setup Routes
+app.use("/", homeRoutes);
+
+//Server Running
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}/, you better catch it!`);
+});
