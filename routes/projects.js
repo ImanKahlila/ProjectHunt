4346ca4a0ea24491f6beeeacc2290a85afbe606e
Andrew Quach <andrewrquach@gmail.com>
2023-05-24 21:53:41 -0700

    Refactor create page GET method to enforce user login requirement

--- routes/projects.js
+++ routes/projects.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const projectController = require("../controllers/projects");
+const { ensureAuth } = require("../middleware/auth");
 
-router.get("/create", projectController.getCreate);
+router.get("/create", ensureAuth, projectController.getCreate);
 router.post("/createProject", projectController.createProject);
 
 module.exports = router;
