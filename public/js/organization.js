8c3ccc080092ff4e5b4b0035d1ed3a52d4a3a1c2
Andrew Quach <andrewrquach@gmail.com>
2023-06-19 18:56:00 -0700

    Enable autofill of organization website input field based on selected organization

--- public/js/organization.js
+++ public/js/organization.js
@@ -1,21 +1,27 @@
-// Retrieve onganizations from mongoDB collection and map to array of names for awesomplete suggestions
+// Retrieve organizations from mongoDB collection and map to array of names for awesomplete suggestions
 
-async function getOrgs(){
-    // Get the autocomplete input element using its ID
-const input = document.getElementById('org-input');
-    try{
-        const response = await fetch('/getOrgs', {
-            method: 'get',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify()
-        })
-        const data = await response.json()
-        const orgNames = data.map(org => org.name)
-        // Initialize the Awesomplete autocomplete on the input element
-        new Awesomplete(input, { list: orgNames });
-    }catch(err){
-        console.log(err)
-    }
+async function getOrgs() {
+  const input = document.getElementById("org-input");
+  const websiteInput = document.getElementById("website"); // Get the organization website input element
+
+  try {
+    const response = await fetch("/getOrgs", {
+      method: "get",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(),
+    });
+    const data = await response.json();
+    const orgNames = data.map((org) => org.name);
+
+    new Awesomplete(input, { list: orgNames });
+
+    input.addEventListener("awesomplete-selectcomplete", function (event) {
+      const selectedOrganization = event.text.value;
+      websiteInput.value = selectedOrganization; // Autofill the website input field with the selected organization
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-getOrgs()
+getOrgs();
