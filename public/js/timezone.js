bd90e6a90434eb8bb2e79c3988b1a49c83727d9c
Andrew Quach <andrewrquach@gmail.com>
2023-07-01 20:09:24 -0700

    Implement technology selection in project creation page

--- public/js/timezone.js
+++ public/js/timezone.js
@@ -1,10 +1,8 @@
-const moment = require('moment-timezone');
-
 // Get the autocomplete input element using its ID
-const input = document.getElementById('timezone-input');
+const input = document.getElementById("timezone-input");
 
-// Retrieve an array of all timezone names using 'moment.tz.names()'
-const timezones = moment.tz.names();
+// Retrieve the list of supported time zones
+const supportedTimeZones = Intl.supportedValuesOf("timeZone");
 
 // Initialize the Awesomplete autocomplete on the input element
-new Awesomplete(input, { list: timezones });
+new Awesomplete(input, { list: supportedTimeZones });
--- routes/projects.js
+++ routes/projects.js
@@ -6,5 +6,6 @@ const { ensureAuth } = require("../middleware/auth");
 router.get("/create", ensureAuth, projectController.getCreate);
 router.post("/createProject", projectController.createProject);
 router.get("/getOrgs", projectController.getOrgs);
+router.get("/getTechnologies", projectController.getTechnologies);
 
 module.exports = router;
