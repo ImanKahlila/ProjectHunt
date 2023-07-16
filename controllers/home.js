6122185b5ad30c641a5658de5f74ce0953d60b1c
Andrew Quach <andrewrquach@gmail.com>
2023-07-16 11:23:28 -0700

    Reorganize CSS files and utilize separate files for individual pages, introduce currentPage variable in each controller for proper rendering of EJS pages, added list of projects in profile page, and implement styles for the project page

--- controllers/home.js
+++ controllers/home.js
@@ -1,17 +1,24 @@
-module.exports = {
-    // Render the home page
-    getIndex: (req, res) => {
-        res.render("index.ejs");
-        // Render the "index.ejs" view
-    },
+const Project = require("../models/Project");
 
-    // Render the user profile page
-    getProfile: async (req, res) => {
-        try {
-            res.render("profile.ejs", { user: req.user});
-            // Render the "profile.ejs" view and pass the user and currentPage variables to the view
-        } catch (err) {
-            console.log(err);
-        }
+module.exports = {
+  // Render the home page
+  getIndex: (req, res) => {
+    res.render("index.ejs", { currentPage: "home" });
+    // Render the "index.ejs" view
+  },
+
+  // Render the user profile page
+  getProfile: async (req, res) => {
+    try {
+      const projects = await Project.find({ creator_id: req.user.id });
+      console.log({ projects });
+      res.render("profile.ejs", {
+        projects: projects,
+        user: req.user,
+        currentPage: "profile",
+      });
+    } catch (err) {
+      console.log(err);
     }
+  },
 };
